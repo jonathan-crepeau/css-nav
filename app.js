947e210124a92aeb95b3e63ac065421b22594c1b
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-23 15:51:53 -0700

    Remove listener exercise complete

--- app.js
+++ app.js
@@ -18,17 +18,35 @@
 
 // SECTION - Event Listener Example: Change Background Color w/ Event Type 'focus' & 'blur', 'mouseover' & 'mouseout':
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-function changeColor() {
+// function changeColor() {
+//     const randomColor = `rgb(${rgbValues()}, ${rgbValues()}, ${rgbValues()})`;
+//     document.body.style.backgroundColor = randomColor;
+// }
+
+// function rgbValues() {
+//     return Math.floor(Math.random() * (255 - 0 + 1) + 0);
+// }
+
+// btn.addEventListener('mouseout', changeColor);
+
+
+
+// SECTION - Remove Listeners
+
+const controller = new AbortController();
+
+btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
     const randomColor = `rgb(${rgbValues()}, ${rgbValues()}, ${rgbValues()})`;
     document.body.style.backgroundColor = randomColor;
-}
+}, { signal: controller.signal });
 
 function rgbValues() {
     return Math.floor(Math.random() * (255 - 0 + 1) + 0);
 }
 
-btn.addEventListener('mouseout', changeColor);
-
-
+console.log('end it!');
+controller.abort();
